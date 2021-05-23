0839447032aabd039f726d3f29e78a58e08eb919
ViktorJava <gipsyscrew@gmail.com>
2021-05-23 17:12:48 +0300

    Job c параметрами #3
    Рефакторинг класса.
    Javadoc.

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -9,8 +9,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -20,14 +18,14 @@ import static org.quartz.TriggerBuilder.*;
 
 /**
  * Выполнение действий с заданной периодичностью.
+ * Job c параметром.
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 20.05.2021
  */
 public class AlertRabbit {
-
-    private Properties properties;
+    private final Properties properties;
 
     public AlertRabbit(Properties properties) {
         this.properties = properties;
@@ -35,36 +33,59 @@ public class AlertRabbit {
 
     public static void main(String[] args) {
         AlertRabbit alertRabbit = new AlertRabbit(getProperties("rabbit.properties"));
-
         try (Connection cn = alertRabbit.initDataBase(alertRabbit.properties)) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-
-            JobDataMap data = new JobDataMap();
-            data.put("store", cn);
-            JobDetail job = newJob(Rabbit.class)
-                    .usingJobData(data)
-                    .build();
-            //Создание расписания.
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(interval(alertRabbit.properties))
-                    .repeatForever();
-            //В триггере можно указать, когда начинать запуск.
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
             //Загрузка задачи и триггера в планировщик
-            scheduler.scheduleJob(job, trigger);
+            scheduler.scheduleJob(getJob(cn), setSheduller(alertRabbit.properties));
             Thread.sleep(10000);
             scheduler.shutdown();
-            System.out.println(cn);
-
+            System.out.println("-ok");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Метод создания Job.
+     *
+     * @param cn Передаём в Job готовый объект Connection.
+     * @return Job object.
+     */
+    private static JobDetail getJob(Connection cn) {
+        JobDataMap data = new JobDataMap();
+        data.put("store", cn);
+        return newJob(Rabbit.class)
+                .usingJobData(data)
+                .build();
+    }
+
+    /**
+     * Метод создания задачи.
+     *
+     * @param pr Объект типа Properties.
+     * @return Trigger необходимый для Scheduler.
+     */
+    private static Trigger setSheduller(Properties pr) {
+        //Создание Scheduler.
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(interval(pr))
+                .repeatForever();
+        //В триггере указываем, когда и как начинать запуск.
+        return newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+    }
+
+    /**
+     * Инициализация базы данных.
+     *
+     * @param properties Объект конфигурации.
+     * @return Объект Connection.
+     * @throws ClassNotFoundException Possible exception.
+     * @throws SQLException           Possible exception.
+     */
     private Connection initDataBase(Properties properties) throws
             ClassNotFoundException, SQLException {
         Class.forName(properties.getProperty("driver-class-name"));
@@ -74,7 +95,22 @@ public class AlertRabbit {
                 properties.getProperty("password"));
     }
 
+    /**
+     * Метод извлечения интервала из конфигурационных данных.
+     *
+     * @param properties Объект типа Properties.
+     * @return Интервал.
+     */
+    public static int interval(Properties properties) {
+        return Integer.parseInt(properties.getProperty("rabbit.interval"));
+    }
 
+    /**
+     * Метод чтения файла конфигурации.
+     *
+     * @param path имя файла конфигурации.
+     * @return Объект конфигурации.
+     */
     private static Properties getProperties(String path) {
         Properties properties = new Properties();
         try (InputStream in = AlertRabbit.class
@@ -88,11 +124,6 @@ public class AlertRabbit {
         return properties;
     }
 
-
-    public static int interval(Properties properties) {
-        return Integer.parseInt(properties.getProperty("rabbit.interval"));
-    }
-
     /**
      * Класс который отрабатывает с периодичностью.
      */
@@ -101,9 +132,13 @@ public class AlertRabbit {
             System.out.println(hashCode());
         }
 
+        /**
+         * Метод исполнения Job.
+         *
+         * @param context Параметры Job.
+         */
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
             Connection cn = (Connection) context
                     .getJobDetail()
                     .getJobDataMap()
