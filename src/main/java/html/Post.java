298495b9d17ba605933f7db85e77fe6683027aea
ViktorJava <gipsyscrew@gmail.com>
2021-05-31 01:36:12 +0300

    Модель данных - Post. #7
    Переделал модель данных поста.

---
+++ src/main/java/html/Post.java
@@ -0,0 +1,66 @@
+package html;
+
+import java.time.LocalDateTime;
+
+/**
+ * Модель данных, описывающая пост на сайте sql.ru.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 29.05.2021
+ */
+public class Post {
+    private int id;
+    private String name;
+    private String text;
+    private String link;
+    private LocalDateTime created;
+
+    public Post(int id, String name, String text, String link, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.text = text;
+        this.link = link;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+}
