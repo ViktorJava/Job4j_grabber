7711017f7ddaa1987a1f0f75b1be3cb2321bb7cd
ViktorJava <gipsyscrew@gmail.com>
2021-06-02 23:48:47 +0300

    Загрузка деталей поста #8
    Написал рабочий код.

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 /**
- * <h2>Класс парсит заданный сайт, в размере 6 страниц.</h2>
+ * <h2>Класс парсит заданный сайт, в размере 2 страниц.</h2>
  * https://www.sql.ru/forum/job-offers
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
@@ -20,7 +20,6 @@ import java.time.LocalDateTime;
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
         SqlRuParse sqlRuParse = new SqlRuParse();
-        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
         String url = "https://www.sql.ru/forum/job-offers/";
         for (int i = 1; i <= 2; i++) {
             sqlRuParse.parsePage(url + i);
@@ -34,6 +33,7 @@ public class SqlRuParse {
      * @throws IOException Возможное исключение.
      */
     private void parsePage(String url) throws IOException {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
         Document doc = Jsoup.connect(url).get();
         Elements row = doc.select(".postslisttopic");
         for (Element td: row) {
@@ -42,20 +42,39 @@ public class SqlRuParse {
             System.out.println(postUrl);
             System.out.println(href.text());
             System.out.println(getDescription(postUrl));
-            Element dateTime = td.parent().child(5);
-            System.out.println(dateTime.text());
+            //Element dateTime = td.parent().child(5);
+            //LocalDateTime ldt = sqlRuDateTimeParser.parse(String.valueOf(dateTime.text()));
+            //System.out.println(dateTime.text());
+            //System.out.println(ldt);
+            System.out.println(date(postUrl));
             //System.out.println(date(href.attr("href")));
         }
     }
 
+    /**
+     * Метод возвращает описание поста.
+     *
+     * @param url Ссылка на пост.
+     * @return Строковое представление поста.
+     * @throws IOException Возможное исключение.
+     */
     private String getDescription(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
         Elements row = doc.select(".msgBody");
         return row.get(1).text();
     }
 
-    private static LocalDateTime date(String str) throws IOException {
-        Document doc = Jsoup.connect(str).get();
-        return null;
+    /**
+     * Метод возвращает дату публикации поста.
+     *
+     * @param url Ссылка на пост.
+     * @return Дата и время публикации поста.
+     * @throws IOException Возможное исключение.
+     */
+    private LocalDateTime date(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(".msgFooter");
+        String data = row.get(0).text().split("\\[")[0].trim();
+        return new SqlRuDateTimeParser().parse(data);
     }
 }
