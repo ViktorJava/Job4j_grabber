5b2b8c000af87c140b082353c4d88151754e69ab
ViktorJava <gipsyscrew@gmail.com>
2021-06-03 23:51:47 +0300

    SqlRuParse #10
    Написал интерфейс описывающий парсинг сайта.

---
+++ src/main/java/Parser.java
@@ -0,0 +1,28 @@
+import html.Post;
+
+import java.util.List;
+
+/**
+ * Интерфейс описывающий парсинг сайта.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 03.06.2021
+ */
+public interface Parser {
+    /**
+     * Загрузка списка всех постов.
+     *
+     * @param link Ссылка на страницу с постами.
+     * @return Список постов.
+     */
+    List<Post> list(String link);
+
+    /**
+     * Метод загрузки деталей одного поста.
+     *
+     * @param link Ссылка на страницу поста.
+     * @return Объект с деталями поста.
+     */
+    Post detail(String link);
+}
