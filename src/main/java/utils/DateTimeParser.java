1c23bdc96ef8335c0d6f27e9ec3e413137c115e1
ViktorJava <gipsyscrew@gmail.com>
2021-06-03 23:46:51 +0300

    Технологический коммит #9
    Добавил документацию интерфейса.

--- src/main/java/utils/DateTimeParser.java
+++ src/main/java/utils/DateTimeParser.java
@@ -3,10 +3,18 @@ package utils;
 import java.time.LocalDateTime;
 
 /**
+ * Интерфейс преобразования строки в дату.
+ *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 24.05.2021
  */
 public interface DateTimeParser {
+    /**
+     * Метод парсинга даты типа String в дату типа LocalDateTime.
+     *
+     * @param parse Дата и время типа String.
+     * @return Дата и время типа LocalDateTime.
+     */
     LocalDateTime parse(String parse);
 }
