e0066ebf5d70201295ab6cd7e331320f5b4723b0
ViktorJava <gipsyscrew@gmail.com>
2021-06-10 23:32:56 +0300

    Web #15
    - Реализовал поддержку web.
    - Вынес в файл конфигурации номер порта прослушиваемого серверным сокетом.

--- src/main/java/html/Grabber.java
+++ src/main/java/html/Grabber.java
@@ -5,6 +5,10 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 
@@ -13,6 +17,8 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 /**
+ * Класс граббера.
+ *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 09.06.2021
@@ -20,16 +26,32 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public class Grabber implements Grab {
     private final Properties cfg = new Properties();
 
+    /**
+     * Метод инициализации хранилища в базе данных.
+     *
+     * @return Объект хранилища.
+     */
     public Store store() {
         return new PsqlStore(cfg);
     }
 
+    /**
+     * Метод создания планировщика.
+     *
+     * @return Объект планировщика.
+     * @throws SchedulerException Possible exception.
+     */
     public Scheduler scheduler() throws SchedulerException {
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
         scheduler.start();
         return scheduler;
     }
 
+    /**
+     * Инициализация проперти.
+     *
+     * @throws IOException Possible exception.
+     */
     public void cfg() throws IOException {
         try (InputStream in = Grabber.class
                 .getClassLoader()
@@ -38,6 +60,14 @@ public class Grabber implements Grab {
         }
     }
 
+    /**
+     * Метод инициализации граббера.
+     *
+     * @param parser    Объект типа Parser.
+     * @param store     Объект типа Store.
+     * @param scheduler Объект типа Scheduler.
+     * @throws SchedulerException Possible exception.
+     */
     @Override
     public void init(Parser parser, Store store, Scheduler scheduler) throws
             SchedulerException {
@@ -55,6 +85,36 @@ public class Grabber implements Grab {
                 .withSchedule(times)
                 .build();
         scheduler.scheduleJob(job, trigger);
+    }
+
+    /**
+     * Метод открывает серверный сокет и выдаёт клиенту содержимое базы данных.
+     *
+     * @param store Объект хранилище в базе данных.
+     */
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post: store.getAll()) {
+                            out.write(post
+                                    .toString()
+                                    .getBytes(Charset.forName("Windows-1251")));
+                            out.write(System
+                                    .lineSeparator()
+                                    .getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     public static class GrabJob implements Job {
@@ -75,5 +135,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new SqlRuParse(), store, scheduler);
+        grab.web(store);
     }
 }
